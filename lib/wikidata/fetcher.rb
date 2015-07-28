f4fedca41bb9f8902b0edd20d05ef9973fef0d06
Tony Bowden <tony@tmtm.com>
2015-07-28 08:26:53 +0100

    Add more fields
    And move them out to class data, rather than setting them up each time
    around.

--- lib/wikidata/fetcher.rb
+++ lib/wikidata/fetcher.rb
@@ -49,36 +49,53 @@ class WikiData
       @id = h[:id] or raise "No ID"
     end
 
+    @@skip = { 
+      'P19' => 'Place of Birth',
+      'P27' => 'Country of Citizenship',
+      'P31' => 'Instance of',
+      'P39' => 'Position Held',
+      'P69' => 'Educated at',
+      'P101' => 'Field of Work',
+      'P103' => 'Native language',
+      'P102' => 'Party',
+      'P106' => 'Occupation', 
+      'P108' => 'Employer', 
+      'P166' => 'Award received', 
+      'P373' => 'Commons category', 
+      'P410' => 'Military rank', 
+      'P463' => 'Member of', 
+      'P607' => 'Conflicts', 
+      'P646' => 'Freebase',
+      'P1344' => 'Participant in',
+      'P1412' => 'Languages',
+      'P1447' => 'SportsReference ID',
+      'P1971' => 'Number of children',
+    }
+
+    @@want = { 
+      'P18' =>  [ 'image', 'url' ],
+      'P21' =>  [ 'gender', 'title' ],
+      'P214' => [ 'identifier__VIAF', 'value' ], 
+      'P227' => [ 'identifier__GND', 'value' ], 
+      'P244' => [ 'identifier__LCAuth', 'value' ], 
+      'P268' => [ 'identifier__BNF', 'value' ], 
+      'P345' => [ 'identifier__IMDB', 'value' ], 
+      'P434' => [ 'identifier__MusicBrainz', 'value' ], 
+      'P569' => [ 'birth_date', 'date', 'to_date', 'to_s' ], 
+      'P570' => [ 'death_date', 'date', 'to_date', 'to_s' ], 
+      'P734' => [ 'family_name', 'title' ],
+      'P735' => [ 'given_name', 'title' ],
+      'P1045' => [ 'identifier__sycomore', 'value' ],
+      'P1186' => [ 'identifier__EuroparlMEP', 'value' ], 
+      'P1808' => [ 'identifier__senatfr', 'value' ], 
+      'P1953' => [ 'identifier__discogs', 'value' ], 
+    }
+    
     def data
       wd = Wikidata::Item.find @id 
       return unless wd && wd.hash.key?('claims')
 
       claims = (wd.hash['claims'] || {}).keys.sort_by { |p| p[1..-1].to_i }
-      skip = { 
-        'P19' => 'Place of Birth',
-        'P27' => 'Country of Citizenship',
-        'P31' => 'Instance of',
-        'P39' => 'Position Held',
-        'P69' => 'Educated at',
-        'P102' => 'Party',
-        'P106' => 'Occupation', 
-        'P646' => 'Freebase',
-        'P1412' => 'Languages',
-        'P1447' => 'SportsReference ID',
-      }
-
-      want = { 
-        'P18' =>  [ 'image', 'url' ],
-        'P21' =>  [ 'gender', 'title' ],
-        'P214' => [ 'identifier__VIAF', 'value' ], 
-        'P268' => [ 'identifier__BNF', 'value' ], 
-        'P569' => [ 'birth_date', 'date', 'to_date', 'to_s' ], 
-        'P570' => [ 'death_date', 'date', 'to_date', 'to_s' ], 
-        'P734' => [ 'family_name', 'title' ],
-        'P735' => [ 'given_name', 'title' ],
-        'P1045' => [ 'identifier__sycomore', 'value' ],
-        'P1808' => [ 'identifier__senatfr', 'value' ], 
-      }
 
       #TODO: other languages
       data = {
@@ -86,12 +103,12 @@ class WikiData
         name: wd.labels['en'].value,
       }
 
-      claims.find_all { |c| !skip[c] && !want[c] }.each do |c|
+      claims.reject { |c| @@skip[c] || @@want[c] }.each do |c|
         puts "Unknown claim: https://www.wikidata.org/wiki/Property:#{c}".red
       end
 
-      claims.find_all { |c| want.key? c }.each do |c|
-        att, meth, *more = want[c]
+      claims.find_all { |c| @@want.key? c }.each do |c|
+        att, meth, *more = @@want[c]
         att = att.to_sym
         data[att] = wd.property(c).send(meth)
         data[att] = more.inject(data[att]) { |acc, n| acc.send(n) }
