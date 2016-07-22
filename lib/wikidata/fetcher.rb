28a8f1389b79a04d9b302730ccbb85f5c5ac46ab
Mark Longair <mhl@pobox.com>
2016-07-22 09:12:02 +0100

    Update code for string-to-symbol changes in wikisnakker
    In everypolitician/wikisnakker@3cdf74c1 the keys used in many hashes
    returned by wikisnakker were switched to symbols. This commit updates
    two places where wikidata-fetcher was still expecting keys to be
    strings.

--- lib/wikidata/fetcher.rb
+++ lib/wikidata/fetcher.rb
@@ -234,13 +234,13 @@ class WikiData
 
       @wd.labels.each do |k, v|
         # remove any bracketed element at the end
-        data["name__#{k.tr('-','_')}".to_sym] = v['value'].sub(/ \(.*?\)$/,'')
+        data["name__#{k.to_s.tr('-','_')}".to_sym] = v[:value].sub(/ \(.*?\)$/,'')
       end
 
       data[:name] = [lang, 'en'].flatten.map { |l| data["name__#{l}".to_sym] }.compact.first
 
       @wd.sitelinks.each do |k, v|
-        data["wikipedia__#{k.sub(/wiki$/,'')}".to_sym] = v.title
+        data["wikipedia__#{k.to_s.sub(/wiki$/,'')}".to_sym] = v.title
       end
 
       # Short-circuit if there are no claims
