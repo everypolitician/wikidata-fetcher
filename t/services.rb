2a5330abb294c3ed24d5b383072aea446eec8bb2
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-10-06 16:16:50 +0100

    Test that the person data comes with a youtube field

--- t/services.rb
+++ t/services.rb
@@ -1,10 +1,16 @@
 require 'test_helper'
 
 describe 'data' do
-  around { |test| VCR.use_cassette('konstantinos', &test) }
-  subject { WikiData::Fetcher.new(id: 'Q312013').data }
+  describe 'when person has special websites' do
+    around { |test| VCR.use_cassette('konstantinos', &test) }
+    subject { WikiData::Fetcher.new(id: 'Q312013').data }
 
-  it 'knows Konstantinos id' do
-    subject[:id].must_equal 'Q312013'
+    it 'knows Konstantinos id' do
+      subject[:id].must_equal 'Q312013'
+    end
+
+    it 'has a YouTube key' do
+      subject.key?(:youtube).must_equal true
+    end
   end
 end
