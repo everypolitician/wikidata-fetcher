bd556f667ca977adb2e7aa9ce256c8f6b117bc20
Tony Bowden <tony@tmtm.com>
2017-04-25 10:46:38 +0100

    Find YouTube and Flickr accounts
    P553s isn't a normal 'website' field: it's a pointer to identifiers on
    various other social media platforms. Extract those account details for
    YouTube and Flickr. We can extend this list later, and possibly even
    move it out to a separate JSON file, like the other properties.

--- t/services.rb
+++ t/services.rb
@@ -1,10 +1,20 @@
 require 'test_helper'
 
 describe 'data' do
-  around { |test| VCR.use_cassette('konstantinos', &test) }
-  subject { WikiData::Fetcher.new(id: 'Q312013').data }
+  describe 'when person has special websites' do
+    around { |test| VCR.use_cassette('konstantinos', &test) }
+    subject { WikiData::Fetcher.new(id: 'Q312013').data }
 
-  it 'knows Konstantinos id' do
-    subject[:id].must_equal 'Q312013'
+    it 'knows Konstantinos id' do
+      subject[:id].must_equal 'Q312013'
+    end
+
+    it 'has a YouTube key' do
+      subject[:identifier__youtube].must_equal 'IKMitsotakis'
+    end
+
+    it 'has a Flickr key' do
+      subject[:identifier__flickr].must_equal 'mitsotakis'
+    end
   end
 end
